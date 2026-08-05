ae6e80e5b7969ebf493274347eaf3b5764f54e7e
Tornai Tibor-Zsolt <tornai.tibor.zsolt@gmail.com>
2026-08-05 15:27:13 +0300

    express Router and Controller pattern

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
-import prisma from './prisma'
+import userRoutes from './routes/users.routes'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({
@@ -13,24 +15,6 @@ app.get('/api/health', (req: Request, res: Response) => {
     timestamp: new Date().toISOString(),
   });
 });
-
-app.get('/api/users', async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.users.findMany();
-
-    res.status(200).json({
-      status: 'success',
-      count: URLSearchParams.length,
-      data: users,
-    });
-  } catch (error) {
-    console.error('Hiba a felhasznalok lekerdezesekor: ', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Hiba a szerverrel felhasznalok lekerdezesekor!',
-    });
-  }
- });
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
