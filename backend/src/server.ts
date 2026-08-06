b7d1643765091343107de8b3346714666bd63953
tornaitiborzsolt <tornai.tibor.zsolt@gmail.com>
2026-08-06 16:59:51 +0300

    Revert "RLY-18 added zod schema validator and made basic validation with it"
    This reverts commit f586cd341b8237332871d2d181c2f441d7a47d29.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
-import rootRouter from './api/routes';
+import userRoutes from './api/users/users.routes';
+import authRoutes from './api/auth/auth.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api', rootRouter);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({
