636f254dabdd045c610856ace3888b92a25a4c0b
tornaitiborzsolt <tornai.tibor.zsolt@gmail.com>
2026-08-06 16:59:51 +0300

    Revert "prisma orm init and test"
    This reverts commit 67dde55ada07a6a5f6e241dfa427d4deb61563f1.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import prisma from './prisma'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,24 +12,6 @@ app.get('/api/health', (req: Request, res: Response) => {
     timestamp: new Date().toISOString(),
   });
 });
-
-app.get('/api/users', async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.users.findMany();
-
-    res.status(200).json({
-      status: 'success',
-      count: URLSearchParams.length,
-      data: users,
-    });
-  } catch (error) {
-    console.error('Hiba a felhasznalok lekerdezesekor: ', error);
-    res.status(500).json({
-      status: 'error',
-      message: 'Hiba a szerverrel felhasznalok lekerdezesekor!',
-    });
-  }
- });
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
