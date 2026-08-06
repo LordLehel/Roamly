f586cd341b8237332871d2d181c2f441d7a47d29
Tornai Tibor-Zsolt <tornai.tibor.zsolt@gmail.com>
2026-08-06 16:27:33 +0300

    RLY-18 added zod schema validator and made basic validation with it

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -1,38 +1,63 @@
 import { Request, Response } from 'express';
 import * as authService from './auth.service';
+import * as validationSchemas from './auth.validation';
 
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const validation = validationSchemas.registerSchema.safeParse(req.body);
 
-        const newUser = await authService.registerUser(username, email, password);
+    if (!validation.success) {
+      res.status(400).json({
+        status: 'error',
+        errors: validation.error.flatten().fieldErrors,
+      });
 
-        res.status(201).json({
-            status: 'success',
-            message: `User created with username: ${newUser.username}!`
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            status: 'error',
-            message: 'Error! There was a problem creating the new User!'
-        });
+      return;
     }
+
+    const { username, email, password } = validation.data;
+
+    const newUser = await authService.registerUser(username, email, password);
+
+    res.status(201).json({
+      status: 'success',
+      message: `User created with username: ${newUser.username}!`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Error! There was a problem creating the new User!',
+    });
+  }
 };
 
 export const loginUser = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const validation = validationSchemas.loginSchema.safeParse(req.body);
 
-        const validLogin = await authService.loginUser(email, password);
-        
-        if(validLogin) {
-            res.status(200).json({ status: 'success', message: 'Logged in succesfully!' });
-        } else {
-            res.status(500).json({ status: 'error', message: 'Incorrect email or password!' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'error', message: 'Error! There was an issue during the login process!' });
+    if (!validation.success) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Invalid email or password!',
+      });
+
+      return;
     }
-}
+
+    const { email, password } = validation.data;
+
+    const validLogin = await authService.loginUser(email, password);
+
+    if (validLogin) {
+      res.status(200).json({ status: 'success', message: `Logged in succesfully as: ${validLogin.username}!` });
+    } else {
+      res.status(500).json({ status: 'error', message: 'Incorrect email or password!' });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ status: 'error', message: 'Error! There was an issue during the login process!' });
+  }
+};
--- backend/src/api/users/users.service.ts
+++ backend/src/api/users/users.service.ts
@@ -2,7 +2,7 @@ import prisma from '../../prisma';
 import { users } from '@prisma/client';
 
 export const getAllUsers = async (): Promise<users[]> => {
-    const users = await prisma.users.findMany();
+  const users = await prisma.users.findMany();
 
-    return users;
-}
+  return users;
+};
