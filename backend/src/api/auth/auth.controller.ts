5f116adfd69b1932b34ea860ab4cb5a8c1359011
Tornai Tibor-Zsolt <tornai.tibor.zsolt@gmail.com>
2026-08-06 16:39:32 +0300

    ran prettier on the code

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -50,7 +50,9 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
     const validLogin = await authService.loginUser(email, password);
 
     if (validLogin) {
-      res.status(200).json({ status: 'success', message: `Logged in succesfully as: ${validLogin.username}!` });
+      res
+        .status(200)
+        .json({ status: 'success', message: `Logged in succesfully as: ${validLogin.username}!` });
     } else {
       res.status(500).json({ status: 'error', message: 'Incorrect email or password!' });
     }
