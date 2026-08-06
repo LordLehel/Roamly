3ee2ac48f99b7abb35d3c3bbd44e57855c968862
Tornai Tibor-Zsolt <tornai.tibor.zsolt@gmail.com>
2026-08-06 13:03:46 +0300

    RLY-18 separating folders by route name in api folder

---
+++ backend/src/api/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import * as authService from './auth.service';
+
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { username, email, password } = req.body;
+
+        const newUser = await authService.registerUser(username, email, password);
+
+        res.status(201).json({
+            status: 'success',
+            message: `User created with username: ${newUser.username}!`
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Error! There was a problem creating the new User!'
+        });
+    }
+};
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { email, password } = req.body;
+
+        const validLogin = await authService.loginUser(email, password);
+        
+        if(validLogin) {
+            res.status(200).json({ status: 'success', message: 'Logged in succesfully!' });
+        } else {
+            res.status(500).json({ status: 'error', message: 'Incorrect email or password!' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', message: 'Error! There was an issue during the login process!' });
+    }
+}
