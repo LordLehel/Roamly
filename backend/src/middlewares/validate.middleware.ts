b454b9c65aa3c9d9635e0b0b0478ffb17bdba414
Tornai Tibor-Zsolt <tornai.tibor.zsolt@gmail.com>
2026-08-07 12:28:53 +0300

    final fix for pr

--- backend/src/middlewares/validate.middleware.ts
+++ backend/src/middlewares/validate.middleware.ts
@@ -1,20 +1,20 @@
-import { Request, Response, NextFunction } from "express";
-import { z } from "zod";
+import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
 
 export const validateData = (schema: z.ZodType) => {
-    return (req: Request, res: Response, next: NextFunction) : void => {
-        const validation = schema.safeParse(req.body);
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const validation = schema.safeParse(req.body);
 
-        if (!validation.success) {
-            res.status(400).json({
-                status: 'error',
-                message: validation.error.flatten().fieldErrors,
-            });
+    if (!validation.success) {
+      res.status(400).json({
+        status: 'error',
+        message: validation.error.flatten().fieldErrors,
+      });
 
-            return;
-        }
-
-        req.body = validation.data;
-        next();
+      return;
     }
-}
+
+    req.body = validation.data;
+    next();
+  };
+};
