1155b28f6bc6f8c5ed8b0fee2a373b8eb83a3040
LordLehel99 <leheljozsi2005@gmail.com>
2026-08-12 12:09:38 +0300

    Components were replaced with Nuxt Ui components where possible. UI layout is ready on /register without backend functionality

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -69,10 +69,11 @@
               variant="outline"
               placeholder="sir_real_99@roamly.com"
               class="w-full"
-              :ui="{ base: errors.email
-                ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors' 
-                : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors'
-                }"
+              :ui="{
+                base: errors.email
+                  ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors'
+                  : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors',
+              }"
             />
           </UFormField>
 
@@ -85,10 +86,11 @@
               variant="outline"
               placeholder="ex. sir_real_99"
               class="w-full"
-              :ui="{ base: errors.username
-                ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors' 
-                : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors'
-                }"
+              :ui="{
+                base: errors.username
+                  ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors'
+                  : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors',
+              }"
             />
           </UFormField>
 
@@ -101,10 +103,11 @@
               variant="outline"
               placeholder="********"
               class="w-full"
-              :ui="{ base: errors.password
-                ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors' 
-                : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors'
-                }"
+              :ui="{
+                base: errors.password
+                  ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors'
+                  : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors',
+              }"
             />
           </UFormField>
 
@@ -117,10 +120,11 @@
               variant="outline"
               placeholder="********"
               class="w-full"
-              :ui="{ base: errors.repeatPassword
-                ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors' 
-                : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors'
-                }"
+              :ui="{
+                base: errors.repeatPassword
+                  ? 'bg-red-950/50 text-red-50 ring-1 ring-red-500 !placeholder-red-400 focus:ring-1 focus:ring-red-500 transition-colors'
+                  : 'bg-green-950/50 text-green-50 ring-1 ring-green-500/50 !placeholder-green-400 focus:ring-1 focus:ring-green-500 transition-colors',
+              }"
             />
           </UFormField>
 
@@ -138,7 +142,7 @@
               color="neutral"
               class="h-10 px-6 text-md ring-1 ring-green-800 bg-green-950/50 hover:bg-green-800 hover:ring- hover:ring-green-500 text-green-50 transition-colors"
               @click="clearForm"
-              />
+            />
 
             <!-- register button -->
             <UButton
@@ -191,10 +195,10 @@ const form = reactive({
 
 // The forms errors
 const errors = reactive({
-    email: false,
-    username: false,
-    password: false,
-    repeatPassword: false,
+  email: false,
+  username: false,
+  password: false,
+  repeatPassword: false,
 });
 
 // The forms error message
@@ -203,29 +207,41 @@ const successMessage = ref('');
 
 //email validation
 const validateEmail = (email: string) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
 // watchers
-watch(() => form.email, () => {
-  errors.email = false;
-  generalErrorMessage.value = '';
-});
+watch(
+  () => form.email,
+  () => {
+    errors.email = false;
+    generalErrorMessage.value = '';
+  },
+);
 
-watch(() => form.username, () => {
-  errors.username = false;
-  generalErrorMessage.value = '';
-});
+watch(
+  () => form.username,
+  () => {
+    errors.username = false;
+    generalErrorMessage.value = '';
+  },
+);
 
-watch(() => form.password, () => {
-  errors.password = false;
-  generalErrorMessage.value = '';
-});
+watch(
+  () => form.password,
+  () => {
+    errors.password = false;
+    generalErrorMessage.value = '';
+  },
+);
 
-watch(() => form.repeatPassword, () => {
-  errors.repeatPassword = false;
-  generalErrorMessage.value = '';
-});
+watch(
+  () => form.repeatPassword,
+  () => {
+    errors.repeatPassword = false;
+    generalErrorMessage.value = '';
+  },
+);
 
 const clearForm = () => {
   form.email = '';
@@ -270,8 +286,7 @@ const handleRegister = async () => {
   if (missingFields.length > 0) {
     generalErrorMessage.value = `Missing fields: ${missingFields.join(', ')}`;
     hasError = true;
-  } 
-  else {
+  } else {
     if (!validateEmail(form.email)) {
       errors.email = true;
       generalErrorMessage.value = 'Helytelen e-mail cím formátum!';
@@ -298,18 +313,21 @@ const handleRegister = async () => {
     });
 
     successMessage.value = response.message || 'Registration successful!';
-    
+
     // Siker esetén átirányítás a login oldalra 2 másodperc múlva
     setTimeout(() => {
       router.push('/login');
     }, 2000);
-
   } catch (err: unknown) {
     const errorObj = err as ApiErrorResponse;
     const backendMessage = errorObj?.data?.message || errorObj?.message || 'Registration failed!';
     generalErrorMessage.value = backendMessage;
 
-    if (backendMessage.toLowerCase().includes('email') || backendMessage.toLowerCase().includes('exist') || backendMessage.toLowerCase().includes('taken')) {
+    if (
+      backendMessage.toLowerCase().includes('email') ||
+      backendMessage.toLowerCase().includes('exist') ||
+      backendMessage.toLowerCase().includes('taken')
+    ) {
       errors.email = true;
     }
   }
