f33388bfc9f73e1c7987665f0caa9cd5fc61b5ec
LordLehel99 <leheljozsi2005@gmail.com>
2026-08-20 14:04:20 +0300

    RLY-33 cache removal implemented after log in/out

--- frontend/app/composables/useAuth.ts
+++ frontend/app/composables/useAuth.ts
@@ -10,18 +10,31 @@ export const useAuth = () => {
     secure: import.meta.env.PROD,
     sameSite: 'lax',
   });
+  const queryCache = useQueryCache();
+  const groupsStore = useGroupsStore();
+
+  const clearAppDataAndCache = () => {
+    queryCache.cancelQueries();
+
+    queryCache.getEntries().forEach((entry) => queryCache.remove(entry));
+
+    groupsStore.closeCreateModal();
+    groupsStore.closeDeleteModal();
+  };
 
   const user = useState<UserOutDto | null>('user_profile', () => null);
 
   const isAuthenticated = computed(() => !!token.value);
 
   const login = async (credentials: LoginFormState) => {
+    clearAppDataAndCache();
     const response = await authService.login(credentials);
     token.value = response.token;
     return response;
   };
 
   const logout = () => {
+    clearAppDataAndCache();
     token.value = null;
     user.value = null;
   };
